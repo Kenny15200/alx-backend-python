c12e3ae5d00ce7cdeb605906bcf7cc0d2cde504d
Kehinde Opaleye <opaleyekehinde70@gmail.com>
2024-05-05 03:06:31 -0500

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Convert Float to String
+"""
+
+def to_str(n: float) -> str:
+    """
+    Returns the string representation of a float.
+
+    Args:
+        n (float): The input float.
+
+    Returns:
+        str: The string representation of the input float.
+    """
+    return str(n)
+
