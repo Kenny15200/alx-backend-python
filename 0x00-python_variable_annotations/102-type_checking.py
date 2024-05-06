4f7db27a01c6906076ece60d910985dab4786c27
Kehinde Opaleye <opaleyekehinde70@gmail.com>
2024-05-06 02:43:09 -0500

    Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python3
 
+"""
+Zoom Array
+
+This module defines a function to zoom into an array by repeating its elements.
+
+"""
+
 from typing import Tuple, Any
 
 def zoom_array(lst: Tuple[Any, ...], factor: int = 2) -> Tuple[Any, ...]:
+    """
+    Zoom into an array by repeating its elements.
+
+    Args:
+        lst (Tuple[Any, ...]): The input tuple.
+        factor (int): The zoom factor. Default is 2.
+
+    Returns:
+        Tuple[Any, ...]: The zoomed-in tuple.
+
+    """
     zoomed_in = tuple(
         item for item in lst
         for i in range(factor)
